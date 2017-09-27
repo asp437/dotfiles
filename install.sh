374d845cc09ad96334f0bdab6e98bd3db7c45a33
Aleksandr Parfenov <a.parfenov@postgrespro.ru>
2017-09-27 21:43:41 +0300

    Update zsh theme

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ ln -f -s $dir/.gitignore_global ~/.gitignore_global
 ln -f -s $dir/.gitconfig ~/.gitconfig
 ln -f -s $dir/.vimrc ~/.vimrc
 ln -f -s $dir/.zshrc ~/.zshrc
+ln -f -s $dir/lambda-mod.zsh-theme ~/.oh-my-zsh/themes/lambda-mod.zsh-theme
 ln -f -s $dir/.tmux.conf ~/.tmux.conf
 
