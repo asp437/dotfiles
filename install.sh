86d0d6e842712de2f1940bc767e67ae9fee8562f
Aleksandr Parfenov <a.parfenov@postgrespro.ru>
2017-10-09 17:23:50 +0300

    Update install script. Auto directory creation for links

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ dir=$(pwd -P)
 ln -f -s $dir/.gitignore_global ~/.gitignore_global
 ln -f -s $dir/.gitconfig ~/.gitconfig
 ln -f -s $dir/.vimrc ~/.vimrc
+mkdir -p ~/.config/nvim/
 ln -f -s $dir/init.vim ~/.config/nvim/init.vim
 ln -f -s $dir/.zshrc ~/.zshrc
+mkdir -p ~/.oh-my-zsh/themes/
 ln -f -s $dir/lambda-mod.zsh-theme ~/.oh-my-zsh/themes/lambda-mod.zsh-theme
 ln -f -s $dir/.tmux.conf ~/.tmux.conf
 
