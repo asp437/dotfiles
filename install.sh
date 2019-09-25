7d768410496ce2253ad3d3140a9af38d518e2abf
Aleksandr Parfenov <asp437@gmail.com>
2019-09-25 13:45:33 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ ln -f -s $dir/.zshrc ~/.zshrc
 mkdir -p ~/.oh-my-zsh/themes/
 ln -f -s $dir/lambda-mod.zsh-theme ~/.oh-my-zsh/themes/lambda-mod.zsh-theme
 ln -f -s $dir/.tmux.conf ~/.tmux.conf
-ln -f -s $dir/dot_config/awesome ~/config/awesome
-ln -f -s $dir/dot_config/colours ~/config/colours
-ln -f -s $dir/dot_config/kitty ~/config/kitty
-ln -f -s $dir/dot_config/mc ~/config/mc
-ln -f -s $dir/dot_config/nvim ~/config/nvim
-ln -f -s $dir/dot_config/systemd ~/config/systemd
+ln -f -s $dir/dot_config/awesome ~/.config/awesome
+ln -f -s $dir/dot_config/colours ~/.config/colours
+ln -f -s $dir/dot_config/kitty ~/.config/kitty
+ln -f -s $dir/dot_config/mc ~/.config/mc
+ln -f -s $dir/dot_config/nvim ~/.config/nvim
+ln -f -s $dir/dot_config/systemd ~/.config/systemd
 
