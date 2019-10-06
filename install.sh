6a10bdd5e106b57bb958b06f7e7d70b05bb8731b
Aleksandr Parfenov <asp437@gmail.com>
2019-10-06 18:33:36 +0300

    More home configs to different dir

--- install.sh
+++ install.sh
@@ -15,13 +15,13 @@ rm -rf ~/.zshrc
 rm -rf ~/.tmux.conf
 rm -rf ~/.oh-my-zsh/themes/lambda-mod.zsh-theme
 
-ln -f -s $dir/.gitignore_global ~/.gitignore_global
-ln -f -s $dir/.gitconfig ~/.gitconfig
-ln -f -s $dir/.vimrc ~/.vimrc
-ln -f -s $dir/.zshrc ~/.zshrc
+ln -f -s $dir/home_configs/.gitignore_global ~/.gitignore_global
+ln -f -s $dir/home_configs/.gitconfig ~/.gitconfig
+ln -f -s $dir/home_configs/.vimrc ~/.vimrc
+ln -f -s $dir/home_configs/.zshrc ~/.zshrc
 mkdir -p ~/.oh-my-zsh/themes/
-ln -f -s $dir/lambda-mod.zsh-theme ~/.oh-my-zsh/themes/lambda-mod.zsh-theme
-ln -f -s $dir/.tmux.conf ~/.tmux.conf
+ln -f -s $dir/home_configs/lambda-mod.zsh-theme ~/.oh-my-zsh/themes/lambda-mod.zsh-theme
+ln -f -s $dir/home_configs/.tmux.conf ~/.tmux.conf
 ln -f -s $dir/dot_config/awesome ~/.config/awesome
 ln -f -s $dir/dot_config/colours ~/.config/colours
 ln -f -s $dir/dot_config/kitty ~/.config/kitty
