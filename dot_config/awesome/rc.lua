700df706420c19c28ad0bf5247281340d41d7068
Aleksandr Parfenov <asp437@gmail.com>
2019-10-13 23:27:50 +0300

    Moved some autostart application to awesome config

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -590,6 +590,8 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
+awful.spawn.with_shell("~/.config/awesome/autostart.sh")
+
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
