4dbd5b72eec0c8e7eed33f6f5dec6940b9e677d4
Aleksandr Parfenov <asp437@gmail.com>
2020-02-03 16:21:25 +0300

    Smaller updates widget with tooltip

--- dot_config/awesome/custom_widgets/updates.lua
+++ dot_config/awesome/custom_widgets/updates.lua
@@ -13,11 +13,26 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 local colours = require("colours")
 
 function createUpdatesWidget()
-    return awful.widget.watch(
-        "/home/asp437/.config/awesome/custom_widgets/arch-update -a -b" .. colours.fg_normal .. " -u" .. colours.text_red,
-        300,
+    status = ''
+    updates_widget = awful.widget.watch(
+        "/home/asp437/.config/awesome/custom_widgets/arch-update -s",
+        30,
         function(widget, stdout)
-            widget:set_markup(' ' .. stdout)
+            output = gears.string.split(stdout, '\n')
+            updates_count = tonumber(output[1])
+            fg_color = colours.fg_normal
+            if updates_count > 0 then
+                fg_color = colours.text_red
+            end
+            widget:set_markup(lain.util.markup(fg_color, '  '))
+            status = output[2] .. '\n' .. output[3]
         end
     )
+    updates_tooltip = awful.tooltip {
+        objects = { updates_widget },
+        timer_function = function()
+            return status
+        end
+    }
+    return updates_widget
 end
