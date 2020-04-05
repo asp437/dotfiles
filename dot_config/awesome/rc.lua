04b3416b77fd16359755cc505498c01b2689199b
Aleksandr Parfenov <asp437@gmail.com>
2020-04-05 13:30:02 +0300

    Change default terminal:

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -60,7 +60,7 @@ beautiful.init("/home/asp437/.config/awesome/theme.lua")
 naughty.config.defaults['icon_size'] = 100
 
 -- This is used later as the default terminal and editor to run.
-terminal = "kitty"
+terminal = "alacritty"
 file_manager = "pcmanfm"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
