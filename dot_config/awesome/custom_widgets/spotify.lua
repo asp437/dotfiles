c9c84a2af9abea5b294399a52e446237a908afcb
Aleksandr Parfenov <asp437@gmail.com>
2020-09-14 22:44:19 +0300

    Some additional things to spotify widget

--- dot_config/awesome/custom_widgets/spotify.lua
+++ dot_config/awesome/custom_widgets/spotify.lua
@@ -9,7 +9,6 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local lain = require("lain")
--- local dbus = require("dbus")
 local hotkeys_popup = require("awful.hotkeys_popup")
 local colours = require("colours")
 
@@ -25,32 +24,34 @@ function createSpotifyWidget()
         )
     }
 
+    local object_path = '/org/mpris/MediaPlayer2'
+
     tooltip = awful.tooltip {
         objects = { widget },
-        markup = 'test'
+        markup = 'No music'
     }
 
     name = 'ru.user.asp437'
     dbus.release_name('session', name)
     local request_succ = dbus.request_name('session', name)
-    local tooltip_text
     gears.debug.dump("request_succ: " .. tostring(request_succ))
     dbus.add_match(
         'session',
-        "type='signal', interface='org.freedesktop.DBus.Properties', path='/org/mpris/MediaPlayer2', member='PropertiesChanged'"
+        "type='signal', sender='org.mpris.MediaPlayer2.spotify', interface='org.freedesktop.DBus.Properties', path='/org/mpris/MediaPlayer2', member='PropertiesChanged'"
     )
+    local tooltip_text
+
     signal_handler = function (...)
-        gears.debug.dump('signal caught')
         local data = {...}
         local content = data[3]
         if content['Metadata'] ~= nil then
             tooltip_text = 'Artist: ' .. tostring(content['Metadata']['xesam:artist'][1]) .. '\n'
             tooltip_text = tooltip_text .. 'Track: ' .. tostring(content['Metadata']['xesam:title']) .. '\n'
-            tooltip_text = tooltip_text .. 'Album: ' .. tostring(content['Metadata']['xesam:album']) .. '\n'
+            tooltip_text = tooltip_text .. 'Album: ' .. tostring(content['Metadata']['xesam:album'])
             tooltip.markup = lain.util.markup(fg_color, tooltip_text)
         end
-        gears.debug.dump(data)
     end
+
     local signal_interface = 'org.freedesktop.DBus.Properties'
     dbus.disconnect_signal(signal_interface, signal_handler)
     dbus.connect_signal(signal_interface, signal_handler )
